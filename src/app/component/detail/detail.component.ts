4e5365292428ee6736d81d31f585c3bb37e5bb94
Vcogo <vanshaj.wore@cogoport.com>
2024-05-27 18:06:57 +0530

    pagination done

--- src/app/component/detail/detail.component.ts
+++ src/app/component/detail/detail.component.ts
@@ -1,6 +1,8 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import { HttpResponse } from '@angular/common/http';
+
 
 
 @Component({
@@ -9,28 +11,64 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent {
-  username= "";
+  username = "";
   userDetail: any;
   repoDetail: any;
+  lastPage = 1;
+  currentPage = 1;
+  itemsPerPage = 10;
+  itemsPerPageOptions = [5, 10, 15, 20];
+
   constructor(private route: ActivatedRoute, private apiService: ApiService
-  ) { 
-    
+  ) {
+
   }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.username = params['user'];
       console.log(this.username)
-      this.apiService.getUser(this.username).subscribe(data=> {
-        this.userDetail=data
-        this.apiService.getRepo(this.userDetail.repos_url).subscribe(data=> {
-          this.repoDetail=data;
-          console.log(this.repoDetail)
-        })
-      });
-      
-
+      this.fetchUserDetails()
       // Fetch details based on itemId
-    });}
+    });
+  }
+
+  fetchUserDetails(): void {
+    this.apiService.getUser(this.username).subscribe((response: HttpResponse<any>) => {
+      this.userDetail = response.body;
+      this.fetchRepoDetails()
+    });
+  }
+
+  fetchRepoDetails(): void {
+    this.apiService.getRepo(this.userDetail.repos_url,this.itemsPerPage,this.currentPage).subscribe((resp: HttpResponse<any>) => {
+      this.repoDetail = resp.body;
+      if (resp.headers.get('Link')) {
+        let link = resp.headers.get('Link')
+        this.lastPage = this.apiService.extractLastPage(link ? link : '')
+        console.log('last page', this.lastPage)
+      }
+    })
+  }
+
+  changeItemsPerPage(event: Event): void {
+    this.itemsPerPage = +(event.target as HTMLSelectElement).value;
+    this.currentPage = 1; // Reset to first page
+    this.fetchRepoDetails();
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.lastPage) {
+      this.currentPage++;
+      this.fetchRepoDetails();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.fetchRepoDetails();
+    }
+  }
 
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { tap, throwError, Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
 
 @Injectable({
@@ -16,20 +16,42 @@ export class ApiService {
       'Authorization': `Bearer ${token}`
     });
   }
-  
-  getUser(githubUsername: string) {
-    return this.httpClient.get(`https://api.github.com/users/${githubUsername}`, { headers: this.headers });
+
+  getUser(githubUsername: string): Observable<HttpResponse<any>> {
+    return this.httpClient.get<any>(`https://api.github.com/users/${githubUsername}`, { headers: this.headers, observe: 'response' });
   }
 
-  getRepo(githubRepo: string) {
+  getRepo(githubRepo: string, per_page: number = 10, page: number = 1): Observable<HttpResponse<any>> {
+    let param = new HttpParams().set('page', page).set('per_page', per_page);
 
-    return this.httpClient.get(githubRepo, { headers: this.headers });
+    return this.httpClient.get<any>(githubRepo, { headers: this.headers, observe: 'response', params: param });
   }
 
   getLanguage(languageLink: string) {
 
-    return this.httpClient.get(languageLink, { headers: this.headers});
+    return this.httpClient.get(languageLink, { headers: this.headers });
   }
+
+  extractLastPage(linkHeader: string): number {
+    if (!linkHeader) return 1;
+
+    const links = linkHeader.split(',');
+    let lastPage = 1;
+
+    links.forEach(link => {
+      const [urlPart, relPart] = link.split(';');
+      const url = urlPart.trim().slice(1, -1);
+      const rel = relPart.trim().split('=')[1].replace(/"/g, '');
+
+      if (rel === 'last') {
+        const urlParams = new URLSearchParams(url.split('?')[1]);
+        lastPage = parseInt(urlParams.get('page') || '1', 10);
+      }
+    });
+
+    return lastPage;
+  }
+
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
 }
